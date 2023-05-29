7c322dd9f0535492117e6b2a1d4540b07235b32e
Gimmy <gmy77@users.noreply.github.com>
2023-05-29 12:49:54 +0200

    improver.js

--- scripts/improver.js
+++ scripts/improver.js
@@ -1,121 +1,41 @@
 /*
-File: improver.js
+File: password_manager.js
 Zachary Muranaka
-Improves the user's password by randomly changing letters into numbers or symbols that look similar
+Generates a random password based on the user's input and can improve it as well
+using the ANU QRNG API https://qrng.anu.edu.au/contact/api-documentation/
 */
 
 // DOM elements
+const numberOfCharsInput = document.getElementById("numberOfChars");
+const numsInput = document.getElementById("nums");
+const mixCaseInput = document.getElementById("mixCase");
+const symbolsInput = document.getElementById("symbols");
+const passwordDisplay = document.getElementById("password");
 const oldPasswordInput = document.getElementById("oldPassword");
 const improvedPasswordDisplay = document.getElementById("improvedPassword");
 
-// Returns a random member of the argument array
-function randomArrayMember(array) { return array[Math.floor(Math.random() * array.length)]; }
+// ASCII codes for lowercase letters, uppercase letters, numbers, and symbols
+const lowerASCII = [...];
+const upperASCII = [...];
+const numASCII = [...];
+const symbolASCII = [...];
 
-// Returns a random character that is the same or looks similar to the argument character
-function getChar(character)
-{
-    switch(character)
-    {
-        case 'a':
-        case 'A':
-            return randomArrayMember(['a', 'A', '4', '@']);
-        case 'b':
-        case 'B':
-            return randomArrayMember(['b', 'B', '8']);
-        case 'c':
-        case 'C':
-            return randomArrayMember(['c', 'C', '<', '(']);
-        case 'd':
-        case 'D':
-            return randomArrayMember(['d', 'D', ')']);
-        case 'e':
-        case 'E':
-            return randomArrayMember(['e', 'E', '3']);
-        case 'f':
-        case 'F':
-            return randomArrayMember(['f', 'F']);
-        case 'g':
-        case 'G':
-            return randomArrayMember(['g', 'G', '6', '9']);
-        case 'h':
-        case 'H':
-            return randomArrayMember(['h', 'H', '#']);
-        case 'i':
-        case 'I':
-            return randomArrayMember(['i', 'I', '1', '!']);
-        case 'j':
-        case 'J':
-            return randomArrayMember(['j', 'J', ']']);
-        case 'k':
-        case 'K':
-            return randomArrayMember(['k', 'K']);
-        case 'l':
-        case 'L':
-            return randomArrayMember(['l', 'L', '1', '|']);
-        case 'm':
-        case 'M':
-            return randomArrayMember(['m', 'M']);
-        case 'n':
-        case 'N':
-            return randomArrayMember(['n', 'N', '^']);
-        case 'o':
-        case 'O':
-            return randomArrayMember(['o', 'O', '0']);
-        case 'p':
-        case 'P':
-            return randomArrayMember(['p', 'P']);
-        case 'q':
-        case 'Q':
-            return randomArrayMember(['q', 'Q']);
-        case 'r':
-        case 'R':
-            return randomArrayMember(['r', 'R']);
-        case 's':
-        case 'S':
-            return randomArrayMember(['s', 'S', '5', '$']);
-        case 't':
-        case 'T':
-            return randomArrayMember(['t', 'T', '7', '+']);
-        case 'u':
-        case 'U':
-            return randomArrayMember(['u', 'U']);
-        case 'v':
-        case 'V':
-            return randomArrayMember(['v', 'V']);
-        case 'w':
-        case 'W':
-            return randomArrayMember(['w', 'W']);
-        case 'x':
-        case 'X':
-            return randomArrayMember(['x', 'X']);
-        case 'y':
-        case 'Y':
-            return randomArrayMember(['y', 'Y']);
-        case 'z':
-        case 'Z':
-            return randomArrayMember(['z', 'Z', '2']);
-        default:
-            return character;
-    }
+async function getPassword() {
+    ...
 }
 
-function getImprovedPassword()
-{
-    let oldPassword = oldPasswordInput.value;
-    improvedPasswordDisplay.innerText = "";
+function randomArrayMember(array) { ... }
 
-    for (let character of oldPassword)
-    {
-        // Write the text of the improved password to the DOM element
-        improvedPasswordDisplay.innerText += getChar(character);
-    }
-
-    if (improvedPasswordDisplay.innerText.length === 0)
-    {
-        // Tell the user to enter a password if they did not enter one
-        improvedPasswordDisplay.innerText = "Please enter a password";
-    }
+function getChar(character) {
+    ...
 }
 
-// Improves the user's password and displays the results when the submit button is clicked
-document.getElementById("submitBtn").addEventListener("click", getImprovedPassword);
+function getImprovedPassword() {
+    ...
+}
+
+// Generate a password when the generate button is clicked
+document.getElementById("generateBtn").addEventListener("click", getPassword);
+
+// Improve the user's password and display the results when the improve button is clicked
+document.getElementById("improveBtn").addEventListener("click", getImprovedPassword);
